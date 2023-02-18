30ec23bc4b9b5b28b190ec8e96753c63d19f9fa2
osun <ocean.sun@sensirion.com>
2023-02-18 14:18:35 +0800

    add library.py

---
+++ python_library.py
@@ -0,0 +1,34 @@
+Python Libraries:
+    国内镜像：
+        1 豆瓣：http://pypi.douban.com/simple/ 
+        2 清华：https://pypi.tuna.tsinghua.edu.cn/simple
+
+    1. Python 2.x/3.x
+    2. Xlrd (.xls)
+    3. Xlwt (.xls)
+    4. openpyxl(.xls)
+    5. Xlutils (.xls)
+    6. xlwings(.xls)
+    7. win32com(.xls,未成功安装)
+    8. MySQL 数据库
+        1. 可能需要预装“微软常用运行库合集”
+    9. mysqlclient
+    10. Pandas
+    11. snaconda
+    12. statsmodels
+    13. scikit-learn
+    14. Matplotlib
+    15. ggplot
+    16. ggplot2(属于R语言的模块）
+    17. Seaborn
+    18. Numpy  是 Python 语言的一个扩展程序库，支持大量的维度数组与矩阵运算，此外也针对数组运算提供大量的数学函数库。
+    19. Scipy
+    20. bs4    筛选html文件内容
+    21. lxml   解析HTML、XML文件
+    22. from selenium import webdriver ：web自动化工具
+    23. from selenium.webdriver.chrome.options import Options  
+    24. requests：
+    25. sensirion_fastedf
+    26. pyinstaller
+        pyinstaller能够在Windows、Linux等操作系统下将Python脚本打包成可直接运行程序。使Python脚本可以在没有安装Python的环境中直接运行，方便共享。
+	27. sensirion_fastedf:盛思锐读取edf文件的库；
